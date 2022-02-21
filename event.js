faed9f7957cbe8e2f23cd9c2015935252146f1e7
Noah <iebenmelu@gmail.com>
2022-02-21 11:07:06 -0800

    added copy and paste feature

--- event.js
+++ event.js
@@ -3,43 +3,59 @@ let fgHexString = "#000000";
 let BGC = [255, 255, 255];
 let FGC = [0, 0, 0];
 
-let body = document.getElementsByTagName('body')[0];
+let body = document.getElementsByTagName("body")[0];
 
-let change = document.getElementById('change');
-let flip = document.getElementById('flip');
+let change = document.getElementById("change");
+let flip = document.getElementById("flip");
+let copy = document.getElementById("copy");
 
-change.addEventListener('click', () => {
-    BGC = generatorRandomColor();
-    FGC = getComplementary(BGC);
+copy.addEventListener("click", () => {
+  copy.innerHTML = "COPIED " + "<i class=\"fas fa-check\">";
+  let toCopy = "";
+  Array.from(document.getElementsByTagName("p")).forEach((text) => {
+    if (text.id === "di") return;
+    toCopy += text.innerText + "\n";
+  });
 
-    bgHexString = convertRGBtoHEX(BGC);
-    fgHexString = convertRGBtoHEX(FGC);
+  navigator.clipboard.writeText(toCopy);
+});
 
-    display(BGC, fgHexString, bgHexString);
-})
+change.addEventListener("click", () => {
+  // reset
+  copy.innerText = "COPY";
+  BGC = generatorRandomColor();
+  FGC = getComplementary(BGC);
 
-flip.addEventListener('click', () => {
-    // swap colors here
-    let [xh, yh] = swap(fgHexString, bgHexString);
-    fgHexString = xh;
-    bgHexString = yh;
+  bgHexString = convertRGBtoHEX(BGC);
+  fgHexString = convertRGBtoHEX(FGC);
 
-    [xh, yh] = swap(FGC, BGC);
-    FGC = xh;
-    BGC = yh;
+  display(BGC, fgHexString, bgHexString);
+});
 
-    display(BGC, fgHexString, bgHexString);
-})
+flip.addEventListener("click", () => {
+  // reset
+  copy.innerText = "COPY";
+  // swap colors here
+  let [xh, yh] = swap(fgHexString, bgHexString);
+  fgHexString = xh;
+  bgHexString = yh;
+
+  [xh, yh] = swap(FGC, BGC);
+  FGC = xh;
+  BGC = yh;
+
+  display(BGC, fgHexString, bgHexString);
+});
 
 /**
- * swaps two data of same type
+ * swaps two data of any type
  *
  * @param x the first element
  * @param y the second element
  * @return an array where `x = swap[0]` and `y = swap[1]`
  */
 function swap(x, y) {
-    return [y, x];
+  return [y, x];
 }
 
 /**
@@ -50,42 +66,39 @@ function swap(x, y) {
  * @param bgHexString the background Hex string
  */
 function display(bg, fgHexString, bgHexString) {
-    let isDark = isColorDark(bg);
+  let isDark = isColorDark(bg);
 
-    Array.from(document.getElementsByTagName('p')).forEach(text => {
+  Array.from(document.getElementsByTagName("p")).forEach((text) => {
+    text.style.color = fgHexString;
+    if (text.id === "fg") text.innerHTML = "Foreground Color: " + fgHexString;
+    else if (text.id === "bg") text.innerHTML = "Background Color: " + bgHexString;
+    else text.innerHTML = "Dark Theme: " + isDark;
 
-        text.style.color = fgHexString;
-        if (text.id === 'fg') text.innerHTML = "Foreground Color: " + fgHexString;
-        else if (text.id === 'bg') text.innerHTML = "Background Color: " + bgHexString;
-        else text.innerHTML = "Dark Theme: " + isDark;
+    // elevate text with an opposite color to enable dark-themed colors display properly
+    if (isDark) {
+      text.classList.add("dark");
+      text.classList.remove("light");
+    } else {
+      text.classList.add("light");
+      text.classList.remove("dark");
+    }
+  });
 
-        // elevate text with an opposite color to enable dark-themed colors display properly
-        if (isDark) {
-            text.classList.add("dark");
-            text.classList.remove("light");
-        } else {
-            text.classList.add("light");
-            text.classList.remove("dark");
-        }
+  Array.from(document.getElementsByTagName("button")).forEach((button) => {
+    button.style.backgroundColor = bgHexString;
+    button.style.color = fgHexString;
 
-    })
+    // elevate text with an opposite color to enable dark-themed colors display properly
+    if (isDark) {
+      button.classList.add("dark");
+      button.classList.remove("light");
+    } else {
+      button.classList.add("light");
+      button.classList.remove("dark");
+    }
+  });
 
-    Array.from(document.getElementsByTagName('button')).forEach(button => {
-
-        button.style.backgroundColor = bgHexString;
-        button.style.color = fgHexString;
-
-        // elevate text with an opposite color to enable dark-themed colors display properly
-        if (isDark) {
-            button.classList.add("dark");
-            button.classList.remove("light");
-        } else {
-            button.classList.add("light");
-            button.classList.remove("dark");
-        }
-    });
-
-    body.style.backgroundColor = bgHexString;
+  body.style.backgroundColor = bgHexString;
 }
 
 /**
@@ -95,16 +108,16 @@ function display(bg, fgHexString, bgHexString) {
  * @returns the color in RGB
  */
 function getComplementary(colors = [0, 0, 0]) {
-    let r = colors[0];
-    let b = colors[1];
-    let g = colors[2];
+  let r = colors[0];
+  let b = colors[1];
+  let g = colors[2];
 
-    let x = Math.max(r, b, g) + Math.min(r, b, g);
+  let x = Math.max(r, b, g) + Math.min(r, b, g);
 
-    let rr = x - r;
-    let bb = x - b;
-    let gg = x - g;
-    return [Math.ceil(rr), Math.floor(bb), Math.floor(gg)];
+  let rr = x - r;
+  let bb = x - b;
+  let gg = x - g;
+  return [Math.ceil(rr), Math.floor(bb), Math.floor(gg)];
 }
 
 /**
@@ -113,10 +126,12 @@ function getComplementary(colors = [0, 0, 0]) {
  * @param the RGB color
  */
 function convertRGBtoHEX(colors = [0, 0, 0]) {
-    return "#" +
-        colors[0].toString(16).padStart(2, "0") +
-        colors[1].toString(16).padStart(2, "0") +
-        colors[2].toString(16).padStart(2, "0");
+  return (
+    "#" +
+    colors[0].toString(16).padStart(2, "0") +
+    colors[1].toString(16).padStart(2, "0") +
+    colors[2].toString(16).padStart(2, "0")
+  );
 }
 
 /**
@@ -125,10 +140,10 @@ function convertRGBtoHEX(colors = [0, 0, 0]) {
  * @returns an array representing the colors
  */
 function generatorRandomColor() {
-    let red = Math.round(Math.random() * 254);
-    let blue = Math.round(Math.random() * 254);
-    let green = Math.round(Math.random() * 254);
-    return [red, blue, green];
+  let red = Math.round(Math.random() * 254);
+  let blue = Math.round(Math.random() * 254);
+  let green = Math.round(Math.random() * 254);
+  return [red, blue, green];
 }
 
 /**
@@ -137,10 +152,10 @@ function generatorRandomColor() {
  * @param the color in RGB
  */
 function isColorDark(colorValues = [0, 0, 0]) {
-    // R.E: http://alienryderflex.com/hsp.html
-    let [red, blue, green] = colorValues;
-    let hsp = Math.sqrt(0.299 * Math.pow(red, 2) + 0.587 * Math.pow(green, 2) + 0.114 * Math.pow(blue, 2));
-    console.log("hsp: " + hsp);
-    if (hsp > 127.5) return false;
-    else return true;
+  // R.E: http://alienryderflex.com/hsp.html
+  let [red, blue, green] = colorValues;
+  let hsp = Math.sqrt(0.299 * Math.pow(red, 2) + 0.587 * Math.pow(green, 2) + 0.114 * Math.pow(blue, 2));
+  console.log("hsp: " + hsp);
+  if (hsp > 127.5) return false;
+  else return true;
 }
