13579fc4c00588d27cacc0b025372b4a8897706b
noahweasley <iebenmelu@gmail.com>
2024-11-20 15:24:47 +0100

    Finish converting to React

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+/* eslint-disable react/prop-types */
+import "./App.css";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import {
+  getComplementary,
+  convertRGBtoHEX,
+  generatorRandomColor,
+  isColorDark,
+} from "./utils.js";
+
+function App() {
+  const [bgColor, setBgColor] = useState([255, 255, 255]);
+  const [fgColor, setFgColor] = useState([0, 0, 0]);
+  const [isCopied1, setIsCopy1] = useState(false);
+  const [isCopied2, setIsCopy2] = useState(false);
+
+  const bgHex = convertRGBtoHEX(bgColor);
+  const fgHex = convertRGBtoHEX(fgColor);
+
+  const handleGenerateColors = () => {
+    const newBgColor = generatorRandomColor();
+    const newFgColor = getComplementary(newBgColor);
+
+    setBgColor(newBgColor);
+    setFgColor(newFgColor);
+
+    isCopied1 && setIsCopy1(false);
+    isCopied2 && setIsCopy2(false);
+  };
+
+  const handleCopy1 = (color) => {
+    navigator.clipboard.writeText(color);
+    setIsCopy1(true);
+  };
+
+  const handleCopy2 = (color) => {
+    navigator.clipboard.writeText(color);
+    setIsCopy2(true);
+  };
+
+  return (
+    <div className="window" onClick={handleGenerateColors}>
+      <ColorContainer
+        fgColor={fgHex}
+        bgColor={bgHex}
+        isCopied={isCopied1}
+        isDark={isColorDark(bgColor)}
+        onCopy={() => handleCopy1(bgHex)}
+      />
+      <ColorContainer
+        fgColor={bgHex}
+        bgColor={fgHex}
+        isCopied={isCopied2}
+        isDark={isColorDark(fgColor)}
+        onCopy={() => handleCopy2(fgHex)}
+      />
+    </div>
+  );
+}
+
+function ColorContainer({ fgColor, bgColor, isDark, onCopy, isCopied }) {
+  return (
+    <div
+      className={`color-container ${isDark ? "dark" : "light"}`}
+      style={{ backgroundColor: bgColor }}
+    >
+      <p style={{ color: fgColor }}>Color Code: {bgColor}</p>
+      <p style={{ color: fgColor }}>
+        {isDark ? "[Dark Color]" : "[Light Color]"}
+      </p>
+      <button
+        className={`btn ${isDark ? "dark" : "light"}`}
+        onClick={(e) => {
+          e.stopPropagation();
+          onCopy();
+        }}
+      >
+        {isCopied ? (
+          <>
+            COPIED <FontAwesomeIcon icon={faCheck} />
+          </>
+        ) : (
+          <>COPY</>
+        )}
+      </button>
+    </div>
+  );
+}
+
+export default App;
