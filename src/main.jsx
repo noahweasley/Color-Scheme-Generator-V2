3e315480c616e8e821594ef5be384c63115dc36f
noahweasley <iebenmelu@gmail.com>
2024-11-25 14:55:10 +0100

    Fix incorrect paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import "./styles/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import App from "./pages/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
